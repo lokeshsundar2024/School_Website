8c1c4ecb822af80440b092a1c06e4a27e4ec989d
unknown <lokesh.sundar@gmail.com>
2026-04-02 09:13:08 +0530

    Initial commit: Next.js deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/School_Website",
   images: {
     unoptimized: true,
     remotePatterns: [
